59e58105bdaf53a5d9b85a0fcadb29e1f41a9b71
leo2111 <thinhphatmai2001@gmail.com>
2022-10-24 23:02:48 +0700

    print ok

--- WATERSORT/datastructure.py
+++ WATERSORT/datastructure.py
@@ -1,11 +1,14 @@
 from collections import deque
+from inspect import stack
 
+# stack data structure
 class Stack:
     def __init__(self,list=[]):
         self.stack = deque(list)
     
     def __call__(self):
-        print(self.stack)
+        
+        return self.stack
 
     def push(self,elem):
         self.stack.appendleft(elem)
@@ -31,7 +34,7 @@ class Stack:
     def length(self):
         return len(self.stack)
     
-
+# Color drop: giọi màu
 class Drop:
     def __init__(self,color,length):
         # self.col = color
@@ -39,7 +42,7 @@ class Drop:
         self.dropColor={'col':color ,'len':length}
     
     def __call__(self):
-        print(self.dropColor)
+        return self.dropColor
     
     def changeColor(self,color):
         self.dropColor['col']=color
@@ -51,11 +54,33 @@ class Drop:
         return self.dropColor['col']
     def length(self):
         return self.dropColor['len']
-        
+  
+d1 = Drop('R',10);
+d2 = Drop('B',5);
+s1 = Stack([d1,d2])
 
+# Glass contain color
 class Glass:
     def __init__(self):
         self.isDone = False
         self.name=''
         self.capacity=0
-        self.tube
+        self.tube = Stack()
+    
+    def __call__(self):
+        print(
+            '{',
+                '\t','name:',self.name,
+                '\n\t','capacity:', self.capacity,
+                '\n\t','Done:',self.isDone,
+                '\n\t','tube: {'
+        );
+        for color in self.tube():
+            print('\t     ',color(),',')
+        print('\t}\n}')
+        
+    
+
+g1 = Glass()
+g1.tube = s1;
+g1()
