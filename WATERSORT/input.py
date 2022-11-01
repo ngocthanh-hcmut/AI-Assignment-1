7ed3be8dd2129abd4ae3f4cf17781860748dbfaa
ngocthanh-hcmut <thanh.ho.it@hcmut.edu.vn>
2022-11-01 13:04:47 +0700

    Change input

--- WATERSORT/input.py
+++ WATERSORT/input.py
@@ -4,13 +4,13 @@ def readInput():
     input = open("Input/1.txt", "r").readlines()
     
     for i in range(len(input)):
-        input[i] = input[i].split(",")
+        input[i] = input[i].split(" ")
     
     capacity = len(input[0])
     
     for i in range(len(input)):
-        input[i][:] = (value for value in input[i] if value != "_")
-    
+        input[i][:] = (value for value in input[i] if (value != "_" and value != "_\n"))
+        
     glasses = []
     for glass in input:
         glasses.append(Glass(capacity, glass))
