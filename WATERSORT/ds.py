aa4c42098d37546e4be397b513c52597fbe8f98e
Leo <thinhphatmai2001@gmail.com>
2022-11-04 14:27:15 +0700

    a

--- WATERSORT/ds.py
+++ WATERSORT/ds.py
@@ -82,9 +82,10 @@ def getHScore(glass):
 class State:
     
     def __init__(self, glasses, parent = None):
-        self.glasses = glasses
+        self.glasses = sorted(glasses)
+        # self.glasses = glasses
         self.parent = parent
-        
+    
     def __eq__(self, other):
         self.glasses
         other.glasses
