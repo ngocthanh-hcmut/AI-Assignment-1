0d90ff6db91631f1bcf0f204b892cadd82030d60
Leo <thinhphatmai2001@gmail.com>
2022-11-05 17:49:37 +0700

    print color v1

--- WATERSORT/main.py
+++ WATERSORT/main.py
@@ -1,37 +1,60 @@
+import os, os.path
 from input import *
 from output import *
 from depthFirstSearch import *
 from aStar import *
 
-# initState = readInput(24)
-# depthResult = depthFirstSearch(initState)
-# AStartResult = aStarSearch(initState)
-# print(depthResult)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+DIR = dir_path + '/Input'
+numOfInput = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+prompt = "Choose the output you want to: \n \t Press 1: Export to txt file in folder Output\n \t Press 2: Export to terminal\n \t Press any key else to quit.\n Your choice:"
+running = True
 
-# # printState(initState)
-# i = 0
-# i = printPath(depthResult,i)
-# j = 0
-# j = printPath(AStartResult,j)
-# print('dept: ',i)
-# print('A*: ',j)
-# for k in range(1, 41):
-#     f = open('output/{}.txt'.format(str(k)),'w')
-#     f.write("New Content")   
-
-for k in range(1, 41):
-    print('\n',k,' ===============================================')
-    depthFile = open('Output/Depth/{}.txt'.format(str(k)),'w')
-    aStartFile = open('Output/AStart/{}.txt'.format(str(k)),'w')
+choice = input(prompt)
+if choice == '1':
     
-    initState = readInput(k)
-    AStartResult = aStarSearch(initState)
-    depthResult = depthFirstSearch(initState)
-    i = 0
-    if(depthResult):
-        i = printPath(depthResult,depthFile)
-    j = 0
-    if(AStartResult):
-        j = printPath(AStartResult,aStartFile)
-    print('dept: ',i)
-    print('A*: ',j)
+    for inputNum in range(1, numOfInput + 1):
+        print('\n ===============================================','\n\nCase-{}:\n'.format(str(inputNum)))
+        depthFile = open('Output/Depth/{}.txt'.format(str(inputNum)),'w')
+        aStartFile = open('Output/AStart/{}.txt'.format(str(inputNum)),'w')
+        
+        initState = readInput(inputNum)
+        AStartResult = aStarSearch(initState)
+        depthResult = depthFirstSearch(initState)
+        depthStep = 0
+        if(depthResult):
+            depthStep = printPath(depthResult,depthFile)
+        AStartStep = 0
+        if(AStartResult):
+            AStartStep = printPath(AStartResult,aStartFile)
+        print('A* Search Step: ',AStartStep)
+        print('depth-first Search Step: ',depthStep)
+    
+elif choice == '2':
+    inp = input("\nthe number of input is 1 to 40. Enter: ")
+    inp = int(inp)
+    searchMethod = input("press 1 A* search\n press 2 depth-first search:")
+    searchMethod = int(searchMethod)
+    initState = readInput(inp)
+    result = None
+    if(searchMethod == 1):
+        result = aStarSearch(initState)
+    else:
+        result = depthFirstSearch(initState)
+    
+    if(result):
+        AStartStep = printPathColor(result)
+        
+    
+    
+    
+    
+else:
+    running = False
+        
+
+
+
+
+
+
--- WATERSORT/output.py
+++ WATERSORT/output.py
@@ -1,5 +1,9 @@
 
-def printState(state, fileName, step):
+# Import required module
+import queue
+from colored import fg
+
+def printState(state,  step=0, fileName = None):
     glasses = state.glasses
     for glass in glasses:
         while (len(glass.colors) < glass.capacity):
@@ -10,15 +14,74 @@ def printState(state, fileName, step):
     for i in range(0, glasses[0].capacity):
         vertical.insert(0, [glass.colors[i] for glass in glasses])
     
-    print("step: "+ str(step) + "\n",file = fileName)
+    print("step: "+ str(step) + ":\n",file = fileName)
     for colors in vertical:
         print("".join(["{:<10}" for i in range(0,len(colors))]).format(*colors),file = fileName)        
+    print("------------------------------------------------------------------------------------------\n", file = fileName)
+    
+def matchColor(color):
+    if color == '___':
+        return fg('black')+color
+    elif color == 'grey':
+        return fg('light_gray')+color
+    elif color ==  'orange':
+        return fg('light_red')+color
+    elif color == 'pink':
+        return fg('light_magenta')+color
+    else:
+        return fg(color)+color
 
+    
+    
+def printStateColors(state,  step=0):
+    glasses = state.glasses
+    for glass in glasses:
+        while (len(glass.colors) < glass.capacity):
+            glass.colors.append("___")
+    
+    vertical = []
+    for i in range(0, glasses[0].capacity):
+        vertical.insert(0, [glass.colors[i] for glass in glasses])
+    
+    print("Step "+ str(step) + ":\n")
+    for colors in vertical:
+        # print("".join(["{:<20}" for i in range(0,len(colors))]).format(*textWithColors(colors)))     
+        print("\t".join([matchColor(color) for color in colors]))
         
-def printPath(state,fileName,step = 0):
-    next = step + 1
-    if (state.parent):
-        next = printPath(state.parent, fileName, step + 1)
-    printState(state, fileName,step)
-    print("------------------------------------------------------------------------------------------", file = fileName)
-    return next
+    print(fg('black')+"------------------------------------------------------------------------------------------\n")
+        
+
+# Initialize LIFO Queue
+StackRepo = queue.LifoQueue()
+        
+def tracePath(state):
+    if state is None:
+        return 
+    else:
+        StackRepo.put(state)
+        tracePath(state.parent)
+
+
+def printPath(goalState,fileName):
+    
+    # trace path and Store in Stack Repo
+    tracePath(goalState)
+    # print out the path
+    step = 0
+    while not StackRepo.empty():
+        state = StackRepo.get()
+        printState(state, step, fileName)
+        step += 1
+    return step
+
+def printPathColor(goalState):
+    # trace path and Store in Stack Repo
+    tracePath(goalState)
+    # print out the path
+    step = 0
+    while not StackRepo.empty():
+        state = StackRepo.get()
+        printStateColors(state, step)
+        step += 1
+    return step    
+    
